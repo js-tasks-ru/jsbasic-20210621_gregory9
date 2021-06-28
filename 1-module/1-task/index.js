1d5a0e53f3df349e7a5ac7ff06e9eed25040729e
Макєєв Григорій <gmakan55@gmail.com>
2021-06-28 19:53:43 +0300

    Task 1 is done

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,18 @@
 function factorial(n) {
-  // ваш код...
+  let resultF = 1;
+  if (n == 0 || n == 1) {
+    resultF = 1;
+  } else {
+
+    for (let i = n; i > 1; i--) {
+      resultF *= i;
+    }
+  }
+
+  return resultF;
 }
+
+factorial(0); // 1
+factorial(1); // 1
+factorial(3); // 6
+factorial(5); // 120
