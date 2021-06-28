cefe065db9e92e896cc803587155dbfa8de03ce4
Макєєв Григорій <gmakan55@gmail.com>
2021-06-28 17:19:44 +0300

    Task4 is done

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,14 @@
 function checkSpam(str) {
-  // ваш код...
+  let lowerStr = str.toLowerCase();      
+      let resultCheck = null;
+      
+      if (lowerStr.includes('1xbet') || lowerStr.includes('xxx')) {
+        return true;
+      } else {
+        return false;
+      }
 }
+
+checkSpam('1XbeT now');  // === true
+checkSpam('free xxxxx');  // === true
+checkSpam('innocent rabbit'); // === false
