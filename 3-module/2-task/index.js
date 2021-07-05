91651857ba11cc4e163c01a120ec8cbf30b1e6ee
Макєєв Григорій <gmakan55@gmail.com>
2021-07-05 19:21:27 +0300

    3-module-2-task is done

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,10 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let arrResult = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= a && arr[i] <= b) {
+      arrResult.push(arr[i]);
+    }
+  }
+  return arrResult;
 }
