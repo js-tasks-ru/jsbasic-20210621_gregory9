2b28b84942f16e99e2c9845aac3e99ae005ad7cb
Макєєв Григорій <gmakan55@gmail.com>
2021-07-05 19:49:22 +0300

    3-module-3-task is done

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');  
+  let arrTemp = [];
+  arrTemp.push(arr[0]);
+
+  for (let i = 1; i < arr.length; i++) {
+    arrTemp.push(arr[i].slice(0, 1).toUpperCase() + arr[i].slice(1));
+  }
+
+  let strNew = arrTemp.join('');
+  //console.log(strNew);
+  return strNew;
 }
