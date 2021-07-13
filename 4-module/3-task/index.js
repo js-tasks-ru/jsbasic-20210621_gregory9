2f5c1d4cf81743813ade7b884227953cc405be86
Макєєв Григорій <gmakan55@gmail.com>
2021-07-13 10:57:27 +0300

    4-module-3-task is done

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+    
+  let rows = Array.from(table.rows).slice(1);
+  
+  for ( let row of rows) {
+
+    if (!row.cells[3].hasAttribute('data-available')) {      
+      row.hidden = true;
+    } 
+
+    if (row.cells[3].hasAttribute('data-available') 
+      && row.cells[3].getAttribute('data-available') == 'true') {       
+      row.classList.add('available');
+    } else if (row.cells[3].hasAttribute('data-available') 
+              && row.cells[3].getAttribute('data-available') == 'false') { 
+      row.classList.add('unavailable');
+    }
+
+    if (row.cells[2].innerHTML == 'm') {    
+      row.classList.add('male');
+    } else if (row.cells[2].innerHTML == 'f') {
+      row.classList.add('female');
+    } 
+    
+    if (row.cells[1].innerHTML < 18) {
+      row.style.cssText = `text-decoration: line-through;`;
+    }
+  }
 }
