5e9009f93afc321d078eac5b474792f910d0b180
Макєєв Григорій <gmakan55@gmail.com>
2021-07-14 12:43:43 +0300

    Fix. 4-module-3-task fixed. data - attributes's values were taken using dataset.

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,27 +1,28 @@
 function highlight(table) {
-    
+
   let rows = Array.from(table.rows).slice(1);
-  
-  for ( let row of rows) {
+  for (let row of rows) {
 
-    if (!row.cells[3].hasAttribute('data-available')) {      
+    if (row.cells[3].dataset.available === undefined) {
       row.hidden = true;
-    } 
+    }
 
-    if (row.cells[3].hasAttribute('data-available') 
-      && row.cells[3].getAttribute('data-available') == 'true') {       
+    if (row.cells[3].dataset.available !== undefined
+      && row.cells[3].dataset.available == 'true') {
       row.classList.add('available');
-    } else if (row.cells[3].hasAttribute('data-available') 
-              && row.cells[3].getAttribute('data-available') == 'false') { 
+    }
+
+    if (row.cells[3].dataset.available !== undefined
+      && row.cells[3].dataset.available == 'false') {
       row.classList.add('unavailable');
     }
 
-    if (row.cells[2].innerHTML == 'm') {    
+    if (row.cells[2].innerHTML == 'm') {
       row.classList.add('male');
     } else if (row.cells[2].innerHTML == 'f') {
       row.classList.add('female');
-    } 
-    
+    }
+
     if (row.cells[1].innerHTML < 18) {
       row.style.cssText = `text-decoration: line-through;`;
     }
