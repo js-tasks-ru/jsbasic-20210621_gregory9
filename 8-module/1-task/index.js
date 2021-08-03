2aa43a0c913c5e0b7aedfb47f671f0f787acffef
Макєєв Григорій <gmakan55@gmail.com>
2021-08-03 16:35:16 +0300

    8-module-1-task is done

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -26,7 +26,7 @@ export default class CartIcon {
       this.elem.classList.add('shake');
       this.elem.addEventListener('transitionend', () => {
         this.elem.classList.remove('shake');
-      }, {once: true});
+      }, { once: true });
 
     } else {
       this.elem.classList.remove('cart-icon_visible');
@@ -39,6 +39,75 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    let offsetWidthCart = this.elem.offsetWidth;
+    //console.log(offsetWidthCart);
+    let offsetHeightCart = this.elem.offsetHeight;
+    //console.log(offsetHeightCart);
+    /* if (offsetWidthCart && offsetHeightCart) {
+      console.log('Cart is visible!');
+    } else {
+      console.log('Cart is NOT visible!');
+    } */
+
+    let topCoordCart = this.elem.getBoundingClientRect().top; // Координаты относительно окна: getBoundingClientRect
+    //console.log(topCoordCart);
+    let pageYOffset = window.pageYOffset;
+    //console.log(pageYOffset); // Текущая прокрутка сверху:
+
+    let initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;  // текущая Y-координата относительно окна + текущая прокрутка
+    //console.log(initialTopCoord);
+
+    if (pageYOffset > topCoordCart) {
+      /* this.elem.style.position = 'fixed';
+      let containerElem = document.querySelector('.container');
+      //console.log(containerElem);
+      let containerElemRight = containerElem.getBoundingClientRect().right + 20;
+      console.log(containerElemRight);
+
+      let windowWidth = document.documentElement.clientWidth;
+      //console.log(windowWidth);
+      let marginCartRigth = document.documentElement.clientWidth - this.elem.offsetWidth - 10;
+      console.log(marginCartRigth);
+
+      let leftIndent = Math.min(containerElemRight, marginCartRigth) + 'px';
+      console.log(leftIndent); */
+
+      // весь код что выше можно заменить строками ниже
+      let leftIndent = Math.min(
+        document.querySelector('.container').getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      ) + 'px'
+
+      Object.assign(this.elem.style, {
+        position: 'fixed',
+        top: '50px',
+        zIndex: 1e3,
+        right: '10px',
+        left: leftIndent
+      });
+
+
+    } else {
+      //this.elem.style.position = 'absolute';
+
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''        
+      });
+    }
+
+    let isMobile = document.documentElement.clientWidth <= 767;
+    console.log(isMobile);
+
+    if (isMobile) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''        
+      });
+    }
   }
 }
